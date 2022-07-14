a7d7dd18693cc6080079f7ffdc2396393744c416
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-14 22:39:27 +0530

    Add comments

---
+++ layer0/cache.js
@@ -0,0 +1,36 @@
+export const API_CACHE_HANDLER = ({ cache, proxy }) => {
+  cache({
+    edge: {
+      maxAgeSeconds: 60 * 60 * 24,
+      // Cache responses even if they contain cache-control: private header
+      // https://docs.layer0.co/guides/caching#private
+      // https://docs.layer0.co/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
+      forcePrivateCaching: true,
+    },
+    browser: {
+      serviceWorkerSeconds: 60 * 60 * 24,
+    },
+  })
+  proxy('api', { path: ':path*' })
+}
+
+export const EDGE_CACHE_HANDLER = ({ cache }) => {
+  cache({
+    edge: {
+      maxAgeSeconds: 60 * 60 * 24,
+    },
+    browser: {
+      serviceWorkerSeconds: 60 * 60 * 24,
+    },
+  })
+}
+
+export const STATIC_CACHE_CONFIG = {
+  browser: {
+    serviceWorkerSeconds: 60 * 60 * 24 * 365,
+  },
+  edge: {
+    maxAgeSeconds: 60 * 60 * 24 * 365,
+    forcePrivateCaching: true,
+  },
+}
--- layer0/routes.ts
+++ layer0/routes.ts
@@ -2,116 +2,77 @@ import { publicPaths } from './publicPaths'
 import { BACKENDS, Router } from '@layer0/core'
 import getPathsToPrerender from './prerenderRequests'
 import { isProductionBuild } from '@layer0/core/environment'
-
-const API_CACHE_HANDLER = ({ cache, proxy }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60,
-      // Cache responses even if they contain cache-control: private header
-      // https://docs.layer0.co/guides/caching#private
-      // https://docs.layer0.co/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
-      forcePrivateCaching: true,
-    },
-    browser: false,
-  })
-  proxy('api', { path: ':path*' })
-}
+import { API_CACHE_HANDLER, STATIC_CACHE_CONFIG, EDGE_CACHE_HANDLER } from './cache'
 
 const router = new Router()
-  .get(
-    {
-      headers: {
-        // Regex to catch multiple hostnames
-        // Any deployment will have a L0 permalink
-        // Don't allow Google bot to crawl it, read more on:
-        // https://docs.layer0.co/guides/cookbook#blocking-search-engine-crawlers
-        host: /layer0.link|layer0-perma.link/,
-      },
+
+router.get(
+  {
+    headers: {
+      // Regex to catch multiple hostnames
+      // Any deployment will have a L0 permalink
+      // Don't allow Google bot to crawl it, read more on:
+      // https://docs.layer0.co/guides/cookbook#blocking-search-engine-crawlers
+      host: /layer0.link|layer0-perma.link/,
     },
-    ({ setResponseHeader }) => {
-      setResponseHeader('x-robots-tag', 'noindex')
-    }
-  )
-  // Pre-render the static home page
-  // By pre-rendering, once the project is deployed
-  // the set of links are visited to warm the cache
-  // for future visits (expected to be the first view for real users)
-  // More on static prerendering: https://docs.layer0.co/guides/static_prerendering
-  .prerender(getPathsToPrerender)
-  // Serve the old Layer0 predefined routes by the latest prefix
-  .match('/__xdn__/:path*', ({ redirect }) => {
-    redirect('/__layer0__/:path*', 301)
-  })
-  // Cache the Layer0 devtools css js and other assets served by L0 by default
-  .match('/__layer0__/:path*', ({ cache }) => {
-    cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 } })
-  })
-  // API (Any backend) caching
-  .match('/l0-api/:path*', API_CACHE_HANDLER)
+  },
+  ({ setResponseHeader }) => {
+    setResponseHeader('x-robots-tag', 'noindex')
+  }
+)
+
+// Pre-render the static home page
+// By pre-rendering, once the project is deployed
+// the set of links are visited to warm the cache
+// for future visits (expected to be the first view for real users)
+// More on static prerendering: https://docs.layer0.co/guides/static_prerendering
+router.prerender(getPathsToPrerender)
+
+// Serve the old Layer0 predefined routes by the latest prefix
+router.match('/__xdn__/:path*', ({ redirect }) => {
+  redirect('/__layer0__/:path*', 301)
+})
+
+// Cache the Layer0 devtools css js and other assets served by L0 by default
+router.match('/__layer0__/:path*', EDGE_CACHE_HANDLER)
+
+// API (Any backend) caching
+router.match('/l0-api/:path*', API_CACHE_HANDLER)
+
+router.match('/', EDGE_CACHE_HANDLER)
+router.match('/about', EDGE_CACHE_HANDLER)
+router.match('/commerce', EDGE_CACHE_HANDLER)
+router.match('/product/:path', EDGE_CACHE_HANDLER)
+
+// Check if Layer0 is building your app for production
+// It's an inbuilt flag to save you from relying
+// on process.env.NODE_ENV === 'production'
 
 if (isProductionBuild()) {
+  // Serve the computed public paths with a STATIC cache configuration
   publicPaths.forEach((i) => {
     router.get(i, ({ cache, serveStatic }) => {
-      cache({
-        browser: {
-          maxAgeSeconds: 0,
-          serviceWorkerSeconds: 60 * 60 * 24 * 365,
-        },
-        edge: {
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          forcePrivateCaching: true,
-        },
-      })
+      cache(STATIC_CACHE_CONFIG)
       serveStatic(`.output/public/${i}`)
     })
   })
+
+  // Injected service worker (in ./build.js for more reference)
+  // To be served with the pre-configured serviceWorker method
   router.get('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('.output/public/service-worker.js')
   })
-  router.match('/', ({ cache }) => {
-    cache({
-      edge: {
-        maxAgeSeconds: 60 * 60 * 24,
-      },
-      browser: {
-        serviceWorkerSeconds: 60 * 60 * 24,
-      },
-    })
-  })
-  router.match('/about', ({ cache }) => {
-    cache({
-      edge: {
-        maxAgeSeconds: 60 * 60 * 24,
-      },
-      browser: {
-        serviceWorkerSeconds: 60 * 60 * 24,
-      },
-    })
-  })
-  router.match('/commerce', ({ cache }) => {
-    cache({
-      edge: {
-        maxAgeSeconds: 60 * 60 * 24,
-      },
-      browser: {
-        serviceWorkerSeconds: 60 * 60 * 24,
-      },
-    })
-  })
-  router.match('/product/:path', ({ cache }) => {
-    cache({
-      edge: {
-        maxAgeSeconds: 60 * 60 * 24,
-      },
-      browser: {
-        serviceWorkerSeconds: 60 * 60 * 24,
-      },
-    })
-  })
+
+  // Every request if not served above,
+  // Shall delegrate to process defined to be in serverless
+  // See ./prod.js for what happens in the serverless
   router.fallback(({ renderWithApp }) => {
     renderWithApp()
   })
-} else {
+}
+// If the app is not in production
+else {
+  // Proxy every request to process in ./dev.js
   router.fallback(({ proxy }) => {
     proxy(BACKENDS.js)
   })
